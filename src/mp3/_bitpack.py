c05b32d744318859bd888a63f21f86dd9cbab0ee
lmb <scahoo@gmail.com>
2012-07-17 17:56:04 +0200

    Support checking default values in bitpack

--- src/mp3/_bitpack.py
+++ src/mp3/_bitpack.py
@@ -1,3 +1,22 @@
+#
+# _bitpack.py -- Fast if limited bit packing and unpacking routines
+# Copyright (C) 2012 Lorenz Bauer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 import math
 import collections
 
@@ -5,6 +24,12 @@ _BITPACK_TYPES = {
     'i' : int,
     'b' : bool
 }
+
+class error(Exception):
+    pass
+
+class invalid_input_error(error):
+    pass
 
 class formatstr(object):
     format = None
@@ -14,7 +39,7 @@ class formatstr(object):
         if isinstance(fmt, str):
             fmt = fmt.split(',')
         elif not isinstance(fmt, collections.Iterable):
-            raise Exception('Format needs to be string or Iterable')
+            raise error('Format needs to be string or Iterable')
 
         self.format = list(self._parse(fmt))
         self.length = reduce(self._sum, self.format, 0)
@@ -25,35 +50,41 @@ class formatstr(object):
             yield t
 
     @staticmethod
-    def _sum(acc, (_, length)):
+    def _sum(acc, (_, length, _1)):
         return acc + length
 
     @classmethod
     def _parse(cls, fmt):
         for chunk in fmt:
+            default = None
+            if '=' in chunk:
+                chunk, default = chunk.rsplit('=', 1)
+                default = default.startswith('0x') and \
+                    int(default, 16) or int(default)
+            
             if chunk == 'b':
                 type, length = 'b', 1
             else:
                 type, length = chunk.split(':', 1)
 
                 if not length.isdigit():
-                    raise Exception('Length has to be a base-10 integer')
+                    raise error('Length has to be a base-10 integer')
 
                 length = int(length)
 
             if not type in _BITPACK_TYPES:
-                raise Exception('Unsupported type: %s' % type)
+                raise error('Unsupported type: %s' % type)
 
-            yield _BITPACK_TYPES[type], length
+            yield _BITPACK_TYPES[type], length, default
 
 def bitpack_into(fmt, buffer, offset, *values):
     fmt = isinstance(fmt, formatstr) and fmt or formatstr(fmt)
 
-    if len(buffer) < offset + fmt.length:
-        raise Exception('Not enough space in buffer')
+    if fmt.length > len(buffer) - offset:
+        raise error('bitpack_into requires a buffer of at least %d bytes' % fmt.length)
 
     i = bit_offset = 0
-    for type, length in fmt:
+    for type, length, _ in fmt:
         bitmask = pow(2, length) - 1
         value = type(values[i]) & bitmask
 
@@ -86,15 +117,16 @@ def bitpack_into(fmt, buffer, offset, *values):
 
         i += 1
 
+#@profile
 def bitunpack_from(fmt, buffer, offset = 0):
     fmt = isinstance(fmt, formatstr) and fmt or formatstr(fmt)
 
-    if len(buffer) < offset + fmt.length:
-        raise Exception('Not enough space in buffer, need %d + %d have %d' % (offset, fmt.length, len(buffer)))
+    if fmt.length > len(buffer) - offset:
+        raise error('bitunpack_from requires a buffer of at least %d bytes' % fmt.length)
 
     values = []
     i = bit_offset = 0
-    for type, length in fmt:
+    for type, length, default in fmt:
         value = 0
         while length > 0:
             # number of bits of the current value in this byte
@@ -117,6 +149,11 @@ def bitunpack_from(fmt, buffer, offset = 0):
             length -= num_bits
 
         i += 1
-        values.append(type(value))
+        value = type(value)
+        
+        if not default is None and value != default:
+            raise invalid_input_error('Parsed value does not match expected default')
+        
+        values.append(value)
 
     return values
